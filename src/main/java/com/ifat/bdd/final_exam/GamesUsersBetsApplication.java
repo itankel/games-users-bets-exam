c1bd3cc2f090416e0f02070a0d73d3974c4f9c3b
itankel <ifat.tankel@gmail.com>
2021-06-16 19:06:16 +0300

    Fix logger + hadoop properties to application.properties , remove the unused remarks , add README.txt

--- src/main/java/com/ifat/bdd/final_exam/GamesUsersBetsApplication.java
+++ src/main/java/com/ifat/bdd/final_exam/GamesUsersBetsApplication.java
@@ -1,7 +1,5 @@
 package com.ifat.bdd.final_exam;
 
-import org.apache.log4j.Level;
-import org.apache.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -10,11 +8,7 @@ import org.springframework.context.ConfigurableApplicationContext;
 public class GamesUsersBetsApplication {
 
     public static void main(String[] args) {
-//        Logger.getLogger("org").setLevel(Level.OFF);
-//        Logger.getLogger("akka").setLevel(Level.OFF);
-
-        ConfigurableApplicationContext context = SpringApplication.run(GamesUsersBetsApplication.class, args);
-        System.out.println();
+        SpringApplication.run(GamesUsersBetsApplication.class, args);
 
     }
 
--- src/main/java/com/ifat/bdd/final_exam/conf/SparkConfigForSpring.java
+++ src/main/java/com/ifat/bdd/final_exam/conf/SparkConfigForSpring.java
@@ -4,16 +4,9 @@ import org.apache.spark.sql.SparkSession;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-/**
- * @author 
- */
 
 @Configuration
 public class SparkConfigForSpring {
-//    {
-//        System.setProperty("hadoop.home.dir", "C:\\Hadoop\\winutils\\");
-//
-//    }
 
     @Bean
     public SparkSession sparkSessionDev(){
